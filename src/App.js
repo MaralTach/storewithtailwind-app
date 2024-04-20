48f931ea91154c59da07e25e8e9abfcc333265a5
MaralTach <yoloten.maral@gmail.com>
2024-04-20 13:31:21 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 
 import './App.css';
+import AppRouter from './router/AppRouter';
+// import Home from './pages/Home';
+// import Login from './pages/Login';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import Products from './pages/Products';
+// import About from './pages/About';
+// import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
-     <h1>Hello</h1>
+    <div>
+      <AppRouter/>
+  {/* <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<Login />}/>
+    <Route path="/home" element={<Home />}/>
+    <Route path="/products" element={<Products />}/>
+    <Route path="/about" element={<About />}/>
+    <Route path="*" element={<NotFound />}/>
+  </Routes>
+  
+  </BrowserRouter> */}
+
+
+    
     </div>
   );
 }
